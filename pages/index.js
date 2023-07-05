075e9b5304c68808fe07e9d923b939ebcfbc5fe9
root <root@HK-AVELL-A52HYB>
2023-07-04 22:24:41 -0300

    FEAT change message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Natália, Rosas são vermelhas, violetas são azuis, esse seu sorriso é o que me seduz ❤️</h1>
+    return (
+      <h1>
+        "Natália, rosas são vermelhas, violetas são azuis, esse seu sorriso é o
+        que me seduz"s<br/> Klug, Henrique.
+      </h1>
+    );
 }
 
 export default Home
