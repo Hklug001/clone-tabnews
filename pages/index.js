9da905adc0b9f619a3024a9af3050694cd03caf4
root <root@HK-AVELL-A52HYB>
2023-07-04 22:25:23 -0300

    FIX  message

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return (
       <h1>
         "Natália, rosas são vermelhas, violetas são azuis, esse seu sorriso é o
-        que me seduz"s<br/> Klug, Henrique.
+        que me seduz"<br/> Klug, Henrique.
       </h1>
     );
 }
