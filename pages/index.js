ab1efdeedff50cb3940d34eded0b50e414b530ab
root <root@HK-AVELL-A52HYB>
2023-07-04 22:13:33 -0300

    FEAT add name to message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Rosas são vermelhas, violetas são azuis, esse seu sorriso é o que me seduz ❤️</h1>
+    return <h1>Natália, Rosas são vermelhas, violetas são azuis, esse seu sorriso é o que me seduz ❤️</h1>
 }
 
 export default Home
