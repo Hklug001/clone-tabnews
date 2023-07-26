8467841671168bb30e53134ccd9664e4c5f3119a
Hklug <henriqueklug@gmail.com>
2023-07-26 20:26:10 -0300

    adiciono scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-function Home(){
-    return (
-      <h1>
-        "Natália, rosas são vermelhas, violetas são azuis, esse seu sorriso é o
-        que me seduz" ❤️<br/> Klug, Henrique.
-      </h1>
-    );
+function Home() {
+  return (
+    <h1>
+      "Natália, rosas são vermelhas, violetas são azuis, esse seu sorriso é o
+      que me seduz" ❤️
+      <br /> Klug, Henrique.
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
